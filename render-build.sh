53cbd21e6e368f3c3b9f96ee238147da31c33bf9
irita1981 <irita1981@gmail.com>
2025-11-16 20:00:03 +0900

    Fix: remove deprecated Puppeteer API from build script

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
 
-export PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
-mkdir -p $PUPPETEER_CACHE_DIR
-
 npm install
-
-# PuppeteerのChromeを直接インストール
-node -e "require('puppeteer').createBrowserFetcher().download('chrome')"
